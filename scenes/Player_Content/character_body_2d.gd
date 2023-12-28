63d758e3bb7cae0af41f06b0f26272787a92a884
Ian Kristona <108767628+ian-kr@users.noreply.github.com>
2023-12-28 18:09:42 -0500

    Holy shit the peice of shit animation works after an 1 hour of pain

--- scenes/Player_Content/character_body_2d.gd
+++ scenes/Player_Content/character_body_2d.gd
@@ -1,22 +1,33 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+@export var SPEED: float = 30.0
+
+var input_direction : Vector2 = Vector2.ZERO
+var facing = "up"
+func _ready():
+	$AnimationPlayer.play("walk_right")
+func get_input():
+	input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * SPEED
 
 func _physics_process(delta):
-
-	# Get the input direction and handle the movement/deceleration.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	direction = Input.get_axis("ui_up", "ui_down")
-	if direction:
-		velocity.y = direction * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-
-	
+	get_input()
+	# Animation
+	if velocity != Vector2.ZERO:  # Check if the player is moving
+		if Input.is_action_just_pressed("up"):
+			$AnimationPlayer.play("walk_up")
+			facing = "up"
+		elif Input.is_action_just_pressed("down"):
+			$AnimationPlayer.play("walk_down")
+			facing = "down"
+		elif Input.is_action_just_pressed("right"):
+			$AnimationPlayer.play("walk_right")
+			facing = "right"
+		elif Input.is_action_just_pressed("left"):
+			$AnimationPlayer.play("walk_left")
+			facing = "left"
+	else:  # Player is not moving, play idle animation
+		$AnimationPlayer.play("idle_"+facing)  # Replace "idle" with your actual idle animation name
 	
 	move_and_slide()
+
