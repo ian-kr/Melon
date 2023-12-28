a69fbb5efe56ec1817aac560ddba2b624ad04ff6
Ian Kristona <108767628+ian-kr@users.noreply.github.com>
2023-12-28 02:54:57 -0500

    Made player movement
    The speed is too fast for now I will change once map is done.

---
+++ scenes/Player_Content/character_body_2d.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+
+func _physics_process(delta):
+
+	# Get the input direction and handle the movement/deceleration.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	direction = Input.get_axis("ui_up", "ui_down")
+	if direction:
+		velocity.y = direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	
+	
+	move_and_slide()
