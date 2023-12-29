3e0e1868957bfea347366ab4ee0f6ac8dc8eef83
Ian Kristona <108767628+ian-kr@users.noreply.github.com>
2023-12-29 11:36:38 -0500

    Created cow animation and added the boss to the farm

--- Boss.gd
+++ Boss.gd
@@ -2,7 +2,12 @@ extends CharacterBody2D
 
 const SPEED : int = 300.0
 	
+var moving : bool = false
+var health : int = 500
+	
+	
+func _ready():
+	$Cow_Animation.play("Cow_Idle")
 
 func _physics_process(delta):
-	
 	move_and_slide()
