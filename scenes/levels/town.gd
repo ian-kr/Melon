caf4d218e253b5840a264de8150d47871b0b7a17
akitsuhoshiyomi <154935992+akitsuhoshiyomi@users.noreply.github.com>
2023-12-29 00:47:47 -0500

    FINALLY ADDED THE FIRST SCENE
    The first scene is added and also resized the floor LOL

--- scenes/levels/town.gd
+++ scenes/levels/town.gd
@@ -1,9 +1,17 @@
 extends Node2D
 
-
+@onready var plr = $Player
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var introText = "res://scenes/TextScript/intro.txt"
+	var currentFile = FileAccess.open(introText,FileAccess.READ)
+	
+	var text = currentFile.get_as_text()
+	text = text.split("\n")
+	text.remove_at(text.size()-1)
+	for line in text:
+		$TextBox.queue(line)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
