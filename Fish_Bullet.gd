6a323a64a3de7757eb42f64303cd51f0ac3af4a7
akitsuhoshiyomi <154935992+akitsuhoshiyomi@users.noreply.github.com>
2023-12-30 17:01:48 -0500

    Pushing fix
    you're welcome

--- Fish_Bullet.gd
+++ Fish_Bullet.gd
@@ -2,17 +2,17 @@ extends CharacterBody2D
 
 var velo = Vector2(1,0)
 var speed : int = 300
-
+@onready var plr = get_node("/root/InsideBarn/Player")
 
 func _physics_process(delta):
 	
-	if $ezkiel.facing == "down":
+	if plr.facing == "down":
 		velo = Vector2(0,1)
-	if $ezkiel.facing == "up":
+	if plr.facing == "up":
 		velo = Vector2(0,-1)
-	if $ezkiel.facing == "right":
+	if plr.facing == "right":
 		velo = Vector2(1,0)
-	if $ezkeil.facing == "left":
+	if plr.facing == "left":
 		velo = Vector2(-1,0)
 	
 	var collision_info = move_and_collide(velo.normalized()*delta*speed)
