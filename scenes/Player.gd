6d774585fa841dd0884d2b45a2a5e8725ddeea28
Ian Kristona <108767628+ian-kr@users.noreply.github.com>
2023-12-30 07:29:45 -0500

    Init

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,11 +1,10 @@
 extends Sprite2D
 
+var player_health : int = 100
 
 # Called when the node enters the scene tree for the first time.
-func _ready():1
-	print("test")
+func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
