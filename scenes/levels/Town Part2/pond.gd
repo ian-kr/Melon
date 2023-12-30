39bc027f0d89f555b3cba0c578c9bd7ee3163f1f
akitsuhoshiyomi <154935992+akitsuhoshiyomi@users.noreply.github.com>
2023-12-30 02:32:37 -0500

    MODERN
    :)

--- scenes/levels/Town Part2/pond.gd
+++ scenes/levels/Town Part2/pond.gd
@@ -1,11 +1,29 @@
 extends Area2D
-
-
+var claimed = false
+var mapfound = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite2D.play() # Replace with function body.
-
+	$CharmRaphael.hide()
+	claimed = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		if mapfound == true:
+			if(Input.is_action_just_pressed("ui_accept")):
+				if claimed == false:
+					claimed = true
+					$CharmRaphael.show()
+					get_node("/root/TownP2/TextBox").queue("There it is!")
+					# Replace with function body.
+					var timer = get_tree().create_timer(3.0)
+					Talisman.talismans.append("Raphael")
+					while (timer.time_left != 0):
+						$CharmRaphael.position = Vector2(get_node("/root/TownP2/Camera2D").position) + Vector2(-5,-50)
+						await get_tree().create_timer(0.1).timeout
+					$CharmRaphael.hide()
---
+++ scenes/levels/Town Part2/treasureMap.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$TreasureMap.hide()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		$TreasureMap.show()
+		get_node("/root/TownP2/TextBox").queue("This is a map. Maybe I should follow it.")
+		get_node("/root/TownP2/TextBox").queue("pause")
+		pause()
+		$TreasureMap.hide()
+		get_node("/root/TownP2/pond").mapfound = true
+
+func pause():
+	#Waits until the pause is empty
+	while(get_node("/root/TownP2/TextBox").textqueue.is_empty() != true):
+		await get_tree().create_timer(0.5).timeout
